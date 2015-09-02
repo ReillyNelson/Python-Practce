dd3ebf83292c1f9c6e30ed8ffa63e224783cdd1e
ReillyNelson <rnelson9_88@hotmail.com>
2015-09-02 00:43:03 -0700

    FizzBuzz Test

---
+++ FizzBuzz_Test.py
@@ -0,0 +1,13 @@
+__author__ = 'Reilly'
+x=range(1,101)
+for number in x:
+    if number%3==0 and number%5==0:
+        print ("FizzBuzz")
+    elif number%3==0:
+        print("Fizz")
+    elif number%5==0:
+        print("Buzz")
+    elif number:
+        print (number)
+
+
